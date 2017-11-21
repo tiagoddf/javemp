fabb14325afbbb28024bd632264092beb886589c
luiz <luiz@luiz-PC.localdomain>
2017-11-21 20:40:26 -0200

    base 0.8 Metade do produtos pronto

---
+++ JavEmp/src/Clientes/beansCliente.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clientes;
+
+
+public class beansCliente {
+    
+    private String nome;
+    private int id;
+    private double saldo;
+    private int desconto;
+    private String pesquisa;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo( double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the desconto
+     */
+    public int getDesconto() {
+        return desconto;
+    }
+
+    /**
+     * @param desconto the desconto to set
+     */
+    public void setDesconto(int desconto) {
+        this.desconto = desconto;
+    }
+
+    /**
+     * @return the pesquisa
+     */
+    public String getPesquisa() {
+        return pesquisa;
+    }
+
+    /**
+     * @param pesquisa the pesquisa to set
+     */
+    public void setPesquisa(String pesquisa) {
+        this.pesquisa = pesquisa;
+    }
+
+  
+    
+    
+}
---
+++ JavEmp/src/Clientes/daoCliente.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clientes;
+
+import DB.ConnectMYSQL;
+import Funcionarios.beansFuncionarios;
+import Funcionarios.daoFuncionarios;
+import Produtos.beansProdutos;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author luiz
+ */
+public class daoCliente {
+        ConnectMYSQL conex = new ConnectMYSQL();
+        beansCliente mod = new beansCliente();
+        
+        
+        public void Salvar(beansCliente mod){
+        conex.conectar();
+        
+        try {
+            PreparedStatement pst = conex.con.prepareStatement("insert into clientes(nome,saldo) values(?,?)");
+            pst.setString(1, mod.getNome());
+            pst.setDouble(2, mod.getSaldo());
+            pst.execute();
+            JOptionPane.showMessageDialog(null, "Dados inseridos com sucesso");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao inserir dados /n"+ex);
+        }
+        conex.desconecta();
+    }
+        
+         public void editar(beansCliente mod){
+      conex.conectar();
+        try {
+            PreparedStatement pst = conex.con.prepareStatement("update clientes set nome=?, saldo=? where id=?"); //alteração
+            pst.setString   (1, mod.getNome());
+            pst.setDouble   (2, mod.getSaldo());
+            pst.setInt      (3, mod.getId());
+            pst.execute();
+            JOptionPane.showMessageDialog(null, "Dados alterados com sucessos");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro na alteração dos dados /nErro:"+ex );
+        }
+        
+      conex.desconecta();
+        
+    }
+         
+         public void excluir(beansCliente mod){
+        conex.conectar();
+        try {
+            PreparedStatement pst = conex.con.prepareStatement("delete from clientes where id=?");
+            pst.setInt(1, mod.getId());
+            pst.execute();
+             JOptionPane.showMessageDialog(null, "Dados excluidos com sucessos");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(daoFuncionarios.class.getName()).log(Level.SEVERE, null, ex);
+             JOptionPane.showMessageDialog(null, "Erro ao excluir dados/nErro:" +ex);
+        }
+        
+        conex.desconecta();
+        
+    }
+         
+         public beansCliente buscar(beansCliente mod){
+        conex.conectar(); 
+        try {  
+            // preencherTabela("select *from funcionarios order by usuario");
+            conex.executaSql("select *from clientes where nome like'%");
+            conex.rs.first();
+            mod.setId(conex.rs.getInt("id"));
+            mod.setNome(conex.rs.getString("nome"));
+            mod.setSaldo(conex.rs.getDouble("saldo"));
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao buscar usuario /nErro"+ex);
+        }       
+        conex.desconecta();       
+        return mod;
+    }        
+    
+}
+        
+        
+    
+    
+
